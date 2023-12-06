ba896fa67cda216fa8c21cdb7d6cd459b33ce796
gwencar <gwendaelle.cardenas@gmail.com>
2023-12-06 15:56:57 +0100

    add getHistoric

--- HopitalColasElieGwendaelle/src/main/java/hopital/dao/DaoVisite.java
+++ HopitalColasElieGwendaelle/src/main/java/hopital/dao/DaoVisite.java
@@ -1,7 +1,10 @@
 package hopital.dao;
 
+import java.util.List;
+
 import hopital.model.Visite;
 
 public interface DaoVisite extends DaoGeneric<Visite, Integer> {
 
+	public List<Visite> findByPatient(Integer key);
 }
